7b5e4b54b49e5431661cc81001ec77e95ffea2e4
bsevgeny@gmail.com <papakita2009>
2018-08-24 12:34:43 +0300

    InjectRandomInt handle beginning

--- src/main/java/my_spring/CleanerImpl.java
+++ src/main/java/my_spring/CleanerImpl.java
@@ -10,6 +10,10 @@ public class CleanerImpl implements Cleaner {
     @InjectRandomInt(min = 3, max = 6)
     private int repeat;
 
+    public CleanerImpl() {
+        System.out.println("repeat = " + repeat);
+    }
+
     @Override
     public void clean() {
         for (int i = 0; i < repeat; i++) {
